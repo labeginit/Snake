37af07dd295fbf9cdbebd4eeec9f3e805eb01d9a
liliia <liliyameister@gmail.com>
2022-01-26 15:03:09 +0100

    added initial snake position

--- js/game.js
+++ js/game.js
@@ -1,9 +1,12 @@
+let snakeLength = [{x: 1, y: 1}];
 frog =generatePosition();
 
+document.getElementById('grid-container').innerHTML = "";
 frogItem = document.createElement('div');
 
 frogItem.style.gridColumnStart = frog.x;
 frogItem.style.gridRowStart = frog.y;
+console.log('frog coordinates: ' + frog.x + ' ' + frog.y);
 
 frogItem.classList.add('frog')
 document.getElementById('grid-container').appendChild(frogItem);
@@ -11,3 +14,17 @@ document.getElementById('grid-container').appendChild(frogItem);
 function generatePosition(){
     return {x: Math.floor(Math.random() * 24) + 1, y: Math.floor(Math.random() * 24) + 1};
 }
+
+
+snakeItem = document.createElement('div');
+snakeItem.style.gridColumnStart = snakeLength[0].x;
+snakeItem.style.gridRowStart = snakeLength[0].y;
+    
+console.log('snake coordinates: ' + snakeLength[0].x + ' ' + snakeLength[0].y);
+
+snakeItem.classList.add('snakehead');
+snakeItem.classList.add('snakebody');
+
+document.getElementById('grid-container').appendChild(snakeItem);
+   
+
