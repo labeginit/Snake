6185c63cd56fbde4887d1d3fc56e9b64ed2bd6d3
liliia <liliyameister@gmail.com>
2022-01-27 10:06:30 +0100

    snake dies and game restarts in one of two random directions

--- js/game.js
+++ js/game.js
@@ -1,8 +1,14 @@
-let initPos = {x: 0, y: 0}; 
-let snakeLength = [{x: 1, y: 1}];
+let direction = {x: 0, y: 1}; 
+let snakePosition = [{x: 1, y: 1}];
 let lastUpdate = 0;
 frog = generatePosition();
 let speed = 5;
+const directions = [{x: 0, y: 1}, {x: 1, y: 0}]  // two possible start directions
+const up = "ArrowUp";
+const down = "ArrowDown";
+const left = "ArrowLeft";
+const right = "ArrowRight";
+
 
 function start(time) {
     window.requestAnimationFrame(start);
@@ -16,37 +22,43 @@ function start(time) {
 window.requestAnimationFrame(start);
 window.addEventListener('keydown', event =>{
   
-    if(event.key == "ArrowLeft") {
-        initPos.x = -1;
-        initPos.y = 0;   
-    } else if (event.key == "ArrowRight"){
-        initPos.x = 1;
-        initPos.y = 0;
-    } else if (event.key == "ArrowUp"){
-        initPos.x = 0;
-        initPos.y = -1;
-    } else if (event.key == "ArrowDown"){
-        initPos.x = 0;
-        initPos.y = 1;
+    if(event.key == left) {
+        direction.x = -1;
+        direction.y = 0;   
+    } else if (event.key == right){
+        direction.x = 1;
+        direction.y = 0;
+    } else if (event.key == up){
+        direction.x = 0;
+        direction.y = -1;
+    } else if (event.key == down){
+        direction.x = 0;
+        direction.y = 1;
     }
  
 });
 
 function playGame(){
+    if(dead(snakePosition)){
+        alert("Game Over!");
+        //place the snake to the beginning and make a new frog
+        direction =  directions[Math.round(Math.random())];     
+        frog = generatePosition();
+    }
 
-    if(snakeLength[0].y === frog.y && snakeLength[0].x ===frog.x){
+    if(snakePosition[0].y === frog.y && snakePosition[0].x ===frog.x){
         // snake can eat and grow but does not die
-        snakeLength.unshift({x: snakeLength[0].x + initPos.x, y: snakeLength[0].y + initPos.y});
+        snakePosition.unshift({x: snakePosition[0].x + direction.x, y: snakePosition[0].y + direction.y});
        // generate a new frog     
        frog = generatePosition();
     }
  
-    for (let i = snakeLength.length - 2; i>=0; i--) { 
-        snakeLength[i+1] = {...snakeLength[i]};
+    for (let i = snakePosition.length - 2; i>=0; i--) { 
+        snakePosition[i+1] = {...snakePosition[i]};
     }
 
-    snakeLength[0].x += initPos.x;
-    snakeLength[0].y += initPos.y;
+    snakePosition[0].x += direction.x;
+    snakePosition[0].y += direction.y;
 
     //without this the whole game field will eventually be colored into the snake colour
     document.getElementById('grid-container').innerHTML = "";
@@ -69,7 +81,7 @@ function addFrog(){
 }
 
 function addMovingSnake(){
-    snakeLength.forEach((element, index)=>{
+    snakePosition.forEach((element, index)=>{
         snakeItem = document.createElement('div');
 
         snakeItem.style.gridColumnStart = element.x;
@@ -86,5 +98,23 @@ function addMovingSnake(){
         document.getElementById('grid-container').appendChild(snakeItem);
         console.log('snake: ' + snakeItem.innerHTML);
     });
+}
+
+function dead(snake) {
+    // hit the wall
+    if(snake[0].x >= 24 || snake[0].x <=0 || snake[0].y >= 24 || snake[0].y <=0){
+        snakePosition = [{x: 1, y: 1}];
+        return true;
+    }
+
+    // has eaten itself
+    for (let i = 1; i < snakePosition.length; i++) {
+        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+            snakePosition = [{x: 1, y: 1}];
+            return true;
+        }
+    }
+        
+    return false;
 }
 
